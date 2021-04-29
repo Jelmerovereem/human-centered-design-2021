2c23bc4b0f43b655d1f1c909dd899296bf7554cf
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-04-29 13:21:10 +0200

    debugging

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -2,6 +2,8 @@ import playAudioFile from "./modules/playAudioFile.js";
 import { intro } from "./modules/utils/audioFiles.js";
 const introEl = document.querySelector(".intro");
 
+window.shouldRecord = true;
+
 document.body.addEventListener("keyup", (e) => {
 	if (e.code === "KeyB") {
 		playAudioFile(intro);
